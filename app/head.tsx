0784e3ef31ff26c023ea3c502a5eac6ef047a29d
adem kalfalı <ademkalfa9@gmail.com>
2023-03-20 12:35:51 +0300

    chatGPT-messenge start

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Head() {
+  return (
+    <>
+      <title>ChatGPT Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
