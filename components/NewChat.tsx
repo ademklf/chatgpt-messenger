938b5bd7498ea5fb3d6784acbc0667bbaf882fdc
adem kalfalı <ademkalfa9@gmail.com>
2023-03-21 14:51:12 +0300

    nextAuth added

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -2,7 +2,7 @@ import { PlusIcon } from "@heroicons/react/24/solid";
 
 function NewChat() {
   return (
-    <div className="border-gray-700 border chatRow">
+    <div className="border-gray-700 border ">
       <PlusIcon className="h-4 w-4" />
       <p>New Chat</p>
     </div>
